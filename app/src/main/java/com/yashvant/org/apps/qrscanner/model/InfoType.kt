4b9d85383dfc66fde62f8d528076f7ab65fbef33
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-23 16:40:10 +0530

    Initial commit

---
+++ app/src/main/java/com/yashvant/org/apps/qrscanner/model/InfoType.kt
@@ -0,0 +1,9 @@
+package com.yashvant.org.apps.qrscanner.model
+
+import androidx.annotation.DrawableRes
+
+data class InfoType(
+    val title:String,
+    @DrawableRes
+    val drawableRes: Int?
+)
