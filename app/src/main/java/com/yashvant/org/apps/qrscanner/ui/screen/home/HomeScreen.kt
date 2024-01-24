7070c75c5f4b4cffc1334f9a19fa03b22bfb1974
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-24 23:37:10 +0530

    Changing status bar

--- app/src/main/java/com/yashvant/org/apps/qrscanner/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/yashvant/org/apps/qrscanner/ui/screen/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,7 +24,24 @@ fun HomeScreen(navController: NavController) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Scaffold(
+            topBar = {
+                Text(stringResource(R.string.app_name))
+            }
+        ) {
+            Button(onClick = {
+                navController.navigate(NavigationItem.Scan.route)
+            }) {
+                Text(stringResource(R.string.scan_code))
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(onClick = {
+                navController.navigate(NavigationItem.Generate.route)
+            }) {
+                Text(stringResource(R.string.generate_qr_code))
+            }
 
+        }
     }
 }
 
