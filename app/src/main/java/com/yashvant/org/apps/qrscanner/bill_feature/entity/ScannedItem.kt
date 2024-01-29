f90a7ba1743f605efd2115affd8bbdb88194bc36
yadavyashvant <yashvantyadav855@gmail.com>
2024-01-29 11:00:38 +0530

    Making implementing room database

---
+++ app/src/main/java/com/yashvant/org/apps/qrscanner/bill_feature/entity/ScannedItem.kt
@@ -0,0 +1,33 @@
+package com.yashvant.org.apps.qrscanner.bill_feature.entity
+
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+
+@Entity
+data class ScannedItem(
+    @PrimaryKey(autoGenerate = true) val itemId: Int = 0,
+    val itemName: String,
+    val itemPrice: Double
+)
+
+// ScannedItemDao.kt
+@Dao
+interface ScannedItemDao {
+    @Insert
+    suspend fun insertItem(item: ScannedItem)
+
+    @Query("SELECT * FROM ScannedItem")
+    suspend fun getAllItems(): List<ScannedItem>
+}
+
+// AppDatabase.kt
+@Database(entities = [ScannedItem::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun scannedItemDao(): ScannedItemDao
+}
+
