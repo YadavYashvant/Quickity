4d6765ce8dbd0694dc46fe4e4a93b4d8a14dc238
yadavyashvant <yashvantyadav855@gmail.com>
2024-02-03 23:47:42 +0530

    adding bill details

--- app/src/main/java/com/yashvant/org/apps/quickity/bill_feature/model/Bill.kt
+++ app/src/main/java/com/yashvant/org/apps/quickity/bill_feature/model/Bill.kt
@@ -1,5 +1,8 @@
 package com.yashvant.org.apps.quickity.bill_feature.model
 
+import androidx.room.Entity
+
+@Entity(tableName = "bill_table")
 data class Bill(
     val id: Long,
     val itemName: String,
