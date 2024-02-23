1ea99459f8d3f0f75d8a0ce6d7cfde70085935ff
yadavyashvant <yashvantyadav855@gmail.com>
2024-02-23 12:02:16 +0530

    deploying on google cloud

--- app/src/main/java/com/yashvant/org/apps/quickity/api_feature/ApiClient.kt
+++ app/src/main/java/com/yashvant/org/apps/quickity/api_feature/ApiClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient{
     //private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://192.168.0.100:8080"
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
