3dcf999bd19aa0d85d2db2db81cb71abd05b4585
yadavyashvant <yashvantyadav855@gmail.com>
2024-03-01 23:21:27 +0530

    improving billcard

--- app/src/main/java/com/yashvant/org/apps/quickity/bill_feature/ui/components/BillCard.kt
+++ app/src/main/java/com/yashvant/org/apps/quickity/bill_feature/ui/components/BillCard.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -75,7 +76,7 @@ fun BillCard(
 
                 Spacer(modifier = Modifier.height(2.dp))
 
-                Text(text = "Price : ₹${bill.price}")
+                Text(text = bill.price.toString(), fontSize =  16.sp, style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.SemiBold, fontStyle = FontStyle.Italic)
 
                 Spacer(modifier = Modifier.height(2.dp))
 
