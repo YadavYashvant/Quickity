7c0acc99c6e2984a5dc734006d5a9a0d27fbb5a8
yadavyashvant <yashvantyadav855@gmail.com>
2024-03-01 22:53:39 +0530

    improving billcard

--- app/src/main/java/com/yashvant/org/apps/quickity/bill_feature/ui/components/BillCard.kt
+++ app/src/main/java/com/yashvant/org/apps/quickity/bill_feature/ui/components/BillCard.kt
@@ -1,13 +1,26 @@
 package ro.alexmamo.roomjetpackcompose.presentation.books.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yashvant.org.apps.quickity.bill_feature.model.Bill
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,6 +62,120 @@ fun BillCard(
             DeleteIcon(
                 deleteBill = deleteBill
             )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardElevation() {
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = Color(0xFFDAE1E7),
+        modifier = Modifier
+            .height(210.dp)
+            .padding(10.dp),
+        shadowElevation = 10.dp
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Surface(
+                    shape = RoundedCornerShape(24.dp),
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color(0xFFD1D5E1)
+                ) {
+                    Text(
+                        text = "New release",
+                        fontSize =  12.sp,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = "Gift Plant",
+                    fontSize =  24.sp,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Text(text = "Price : 300$")
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "4.0",
+                        fontSize =  14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Icon(
+                        painter = painterResource(id = R),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_star_outline_24),
+                        tint = Color(0xFFF6B266),
+                        contentDescription = null
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                OutlinedButton(
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        containerColor = Color.White
+                    ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(
+                        text = "Read More",
+                        fontSize =  11.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+
+            Surface(
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.size(width = 100.dp, height = 140.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.quickity_navicon),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+            }
         }
     }
 }
