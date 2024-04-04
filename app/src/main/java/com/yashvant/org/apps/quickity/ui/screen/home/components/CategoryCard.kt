0e9154f653a0a718c0f262de8adf281e27146203
yadavyashvant <yashvantyadav855@gmail.com>
2024-04-04 22:50:48 +0530

    fixing category card

--- app/src/main/java/com/yashvant/org/apps/quickity/ui/screen/home/components/CategoryCard.kt
+++ app/src/main/java/com/yashvant/org/apps/quickity/ui/screen/home/components/CategoryCard.kt
@@ -2,6 +2,7 @@ package com.yashvant.org.apps.quickity.ui.screen.home.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -15,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -38,15 +40,25 @@ fun CategoryCard() {
         shape = RoundedCornerShape(20.dp),
         onClick = {}
     ) {
-        Box {
+        Column {
             Image(
-                painter = painterResource(id = R.drawable.grocery1), contentDescription = "grocery",
+                painter = painterResource(id = R.drawable.grocery),
+                contentDescription = "Grocery",
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+//                    .fillMaxSize()
+                    .height(220.dp)
+                    .clip(shape = RoundedCornerShape(20.dp))
             )
 
-            Text(text = "GROCERY", fontFamily = barlowext, fontSize = 36.sp, color = blueV, fontWeight = FontWeight.ExtraBold, modifier = Modifier.align(
-                Alignment.Center))
+            Text(text = "GROCERY",
+                fontFamily = barlowext,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier.align(
+                Alignment.CenterHorizontally)
+                .padding(top = 20.dp)
+            )
         }
     }
 }
