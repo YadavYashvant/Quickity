47087166e7690274a2e88e73aa7e57be0a559ff5
yadavyashvant <yashvantyadav855@gmail.com>
2024-04-04 23:22:32 +0530

    adding animation

---
+++ app/src/main/java/com/yashvant/org/apps/quickity/animation/JetLoader.kt
@@ -0,0 +1,33 @@
+package com.yashvant.org.apps.quickity.animation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.yashvant.org.apps.qrscanner.R
+
+
+@Composable
+fun AnimatedPreloaderJet(modifier: Modifier = Modifier) {
+    val preloaderLottieComposition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(
+            R.raw.jetloading
+        )
+    )
+
+    val preloaderProgress by animateLottieCompositionAsState(
+        preloaderLottieComposition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = true
+    )
+
+    LottieAnimation(
+        composition = preloaderLottieComposition,
+        progress = preloaderProgress,
+        modifier = modifier
+    )
+}
